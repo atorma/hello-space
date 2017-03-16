31291e578a7b710c33b86fb9f064fc507c0a0e94
atorma <anssi.torma@iki.fi>
2017-03-16 23:34:43 +0200

    Another velocity controller test

--- src/velocity_controller.spec.ts
+++ src/velocity_controller.spec.ts
@@ -1,4 +1,4 @@
-import {RunPhysics, Controls, WorldState, ControlParams} from "./external/physics";
+import {RunPhysics, Controls, WorldState, ControlParams, RocketState} from "./external/physics";
 import {InitialState} from "./external/initialstate";
 import {VelocityController} from "./velocity_controller";
 import {Vec3} from "cannon";
@@ -14,12 +14,32 @@ describe('VelocityController', () => {
     });
 
     it('drives the rocket to the target velocity vector from rest', () => {
+        const rocket: RocketState = createRocketState({velocity: new Vec3(0, 0, 0)});
         const targetVelocity: Vec3 = new Vec3(8, 5, 1);
 
         const controller = new VelocityController();
         controller.setTarget(targetVelocity);
 
-        let world: WorldState = new WorldState(createRocketState(), []);
+        let world: WorldState = new WorldState(rocket, []);
+        for (let i = 0; i < 200; i++) {
+            const controlParams: ControlParams = controller.update(world.rocket);
+            const controls: Controls = new Controls(controlParams);
+            world = RunPhysics(world, controls);
+        }
+
+        expect(world.rocket.velocity.norm()/targetVelocity.norm()).toBeLessThan(1.01);
+        expect(world.rocket.velocity.norm()/targetVelocity.norm()).toBeGreaterThan(0.99);
+        expect(radiansToDegrees(getAngleRadiansBetween(world.rocket.velocity, targetVelocity))).toBeLessThan(1);
+    });
+
+    it('drives the rocket to the target velocity vector from movement', () => {
+        const rocket: RocketState = createRocketState({velocity: new Vec3(-2, -1, 5)});
+        const targetVelocity: Vec3 = new Vec3(8, 5, 1);
+
+        const controller = new VelocityController();
+        controller.setTarget(targetVelocity);
+
+        let world: WorldState = new WorldState(rocket, []);
         for (let i = 0; i < 200; i++) {
             const controlParams: ControlParams = controller.update(world.rocket);
             const controls: Controls = new Controls(controlParams);
